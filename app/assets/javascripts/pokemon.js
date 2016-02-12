12d59b7b7cf8c04edff0f2d81aab5c7a1e99f98e
christiano57 <christianfoliver@gmail.com>
2016-02-11 19:24:04 -0500

    added descriptions to modal

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -25,53 +25,72 @@ PokemonApp.Pokemon.prototype.render = function() {
 			self.info = response;
 			console.log("Pokemon info:");
 			console.log(response);
-			function displayTypes (arrayOfTypes) {
-				if (arrayOfTypes[1] == undefined) {
-					return arrayOfTypes[0].name
-				} else {
-					console.log(arrayOfTypes[0].name)
-					return arrayOfTypes[0].name + " and " + arrayOfTypes[1].name
-				}	
-			}
-		$.ajax({
-			url: 'http://pokeapi.co' + self.info.sprites[0].resource_uri,
-
-			success: function (response) {
-				var image = response.image
-				$('.js-pkmn-sprites').html(`<img src="http://pokeapi.co${image}">`);
-			}
-		});
-	// description request
-		// $.ajax({
-		// 		url: 'http://pokeapi.co' + self.info.description,
-		// 		success: function (response) {
-		// 			console.log(response);
-		// 			// $('.js-pkmn-sprites').html(`<img src="http://pokeapi.co${image}">`);
-		// 		}
-
-		// });
-
-			$('.js-pkmn-name').text(self.info.name);
-			$('.js-pkmn-number').text(self.info.pkdx_id);
-			$('.js-pkmn-height').text(self.info.height);
-			$('.js-pkmn-weight').text(self.info.weight);
-			$('.js-pkmn-hp').text(self.info.hp);
-			$('.js-pkmn-attack').text(self.info.attack);
-			$('.js-pkmn-defense').text(self.info.defense);
-			$('.js-pkmn-sp_atk').text(self.info.sp_atk);
-			$('.js-pkmn-sp_def').text(self.info.sp_def);
-			$('.js-pkmn-speed').text(self.info.speed);
-			$('.js-pkmn-types').text(displayTypes(self.info.types));
-
-			$(".js-pokemon-modal").modal("show");
+		getSprite(self.info);
+		showModal(self.info);
+		showDescription(self.info);
 	},
 		error: function () {
 			alert("this is an error");
 		}
 	});
 }
+
+
 PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
 	var uriSegments = pokemonUri.split("/");
 	var secondLast = uriSegments.length - 2;
 	return uriSegments[secondLast];
 };
+
+function showDescription (self) {
+	var arrOfDescripUri = []
+	self.descriptions.forEach( function(description) {
+		arrOfDescripUri.push(description.resource_uri)
+	})
+	var sortedDescriptions = arrOfDescripUri.sort();
+	var descriptionToShow = sortedDescriptions[sortedDescriptions.length - 1]
+	// console.log(descriptionToShow);
+
+		$.ajax({
+				url: 'http://pokeapi.co' + descriptionToShow,
+				success: function (response) {
+					console.log(response);
+					$('.js-pkmn-description').text(response.description);
+				}
+		});
+}
+function getSprite (self) {
+	$.ajax({
+		url: 'http://pokeapi.co' + self.sprites[0].resource_uri,
+
+		success: function (response) {
+			var image = response.image
+			$('.js-pkmn-sprites').html(`<img src="http://pokeapi.co${image}">`);
+		}
+	});
+}
+
+function displayTypes (arrayOfTypes) {
+	if (arrayOfTypes[1] == undefined) {
+		return arrayOfTypes[0].name
+	} else {
+		console.log(arrayOfTypes[0].name)
+		return arrayOfTypes[0].name + " and " + arrayOfTypes[1].name
+	}	
+}
+
+function showModal(self) {
+
+	$('.js-pkmn-name').text(self.name);
+	$('.js-pkmn-number').text(self.pkdx_id);
+	$('.js-pkmn-height').text(self.height);
+	$('.js-pkmn-weight').text(self.weight);
+	$('.js-pkmn-hp').text(self.hp);
+	$('.js-pkmn-attack').text(self.attack);
+	$('.js-pkmn-defense').text(self.defense);
+	$('.js-pkmn-sp_atk').text(self.sp_atk);
+	$('.js-pkmn-sp_def').text(self.sp_def);
+	$('.js-pkmn-speed').text(self.speed);
+	$('.js-pkmn-types').text(displayTypes(self.types));
+	$(".js-pokemon-modal").modal("show");
+}
